f5e8481ad5f6f0dccd9672ef0d95561f6ad01944
keivch <sebastian.hernandez.scarpetta@correounivalle.edu.co>
2024-09-30 14:59:58 -0500

    functional login with google

--- nicelook_api/social_auth_views.py
+++ nicelook_api/social_auth_views.py
@@ -4,43 +4,53 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import get_user_model
 import requests
+from administrator.models import Administrator
 
 User = get_user_model()
 
 class GoogleLogin(SocialLoginView):
-    adapter_class = GoogleOAuth2Adapter
-
     def post(self, request, *args, **kwargs):
         token = request.data.get('token')
 
-        # Enviar token a la API de Google para obtener información del usuario
-        user_info_url = 'https://www.googleapis.com/oauth2/v3/userinfo'
-        headers = {'Authorization': f'Bearer {token}'}
-        user_info_response = requests.get(user_info_url, headers=headers)
+        # Validar el ID Token usando Google API
+        token_info_url = f'https://oauth2.googleapis.com/tokeninfo?id_token={token}'
+        token_info_response = requests.get(token_info_url)
 
-        if user_info_response.status_code == 200:
-            user_data = user_info_response.json()
-            email = user_data.get('email')
-            first_name = user_data.get('given_name')
-            last_name = user_data.get('family_name')
-            google_id = user_data.get('sub')
+        if token_info_response.status_code != 200:
+            return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
 
-            # Obtener o crear el usuario en la base de datos
-            user, created = User.objects.get_or_create(
-                email=email,
-                defaults={
-                    'first_name': first_name,
-                    'last_name': last_name,
-                }
-            )
+        # Si el token es válido, obtener los datos del usuario
+        token_info = token_info_response.json()
+        email = token_info.get('email')
+        first_name = token_info.get('given_name')
+        last_name = token_info.get('family_name')
+        google_id = token_info.get('sub')
 
-            if created:
-                # Si el usuario es creado, puede que desees enviar un correo de bienvenida u otros procesos.
-                pass
+        # Verificar si el correo electrónico está verificado
+        if not token_info.get('email_verified'):
+            return Response({'error': 'Email not verified'}, status=status.HTTP_400_BAD_REQUEST)
 
-            # Puedes autenticar al usuario aquí, si lo deseas
-            # (opcional) login(request, user)
+        # Obtener o crear el usuario en la base de datos
+        user, created = User.objects.get_or_create(
+            email=email,
+            defaults={
+                'first_name': first_name,
+                'last_name': last_name,
+            }
+        )
 
-            return Response({'email': email, 'first_name': first_name, 'last_name': last_name, 'google_id': google_id})
+        if created:
+            Administrator.objects.create(user=user)
+            
+
+        # (Opcional) Autenticar al usuario
+        # login(request, user)
         
-        return Response({'error': 'Unable to authenticate'}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({
+            'email': email, 
+            'first_name': first_name, 
+            'last_name': last_name, 
+            'google_id': google_id,
+            'picture': token_info.get('picture')# También puedes devolver la imagen del perfil
+        }, status=status.HTTP_200_OK)
--- venvN/Scripts/activate.bat
+++ venvN/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\USUARIO\Documents\git\backend_nicelook\venvN
+set VIRTUAL_ENV=C:\Users\aprendiz.sistemas\Desktop\git\Stylos\backend_nicelook\venvN
 
 if not defined PROMPT set PROMPT=$P$G
 
