58a99e988a9fa1bce2343716f0ef008284cb1295
Jhostin Paz <jhostinpaz9@gmail.com>
2024-10-09 01:16:56 -0500

    Endpoint para enlistar citas

--- appointment/views.py
+++ appointment/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets,status
 from .models import Appointment
 from .serializers import appointmentSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 # Create your views here.
 class appointmentViewSet(viewsets.ModelViewSet):
     serializer_class = appointmentSerializer
     queryset = Appointment.objects.all()
     
+@api_view(['GET'])
+def appointment_list(request):
+    try:
+        appointments = Appointment.objects.all()
+        serializer = appointmentSerializer(appointments, many=True)
+        return Response(serializer.data)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
--- client/serializers.py
+++ client/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Client
 
 class clientSerializer(serializers.ModelSerializer):
+    user= serializers.StringRelatedField()
     class Meta:
         model = Client
         fields = '__all__'
