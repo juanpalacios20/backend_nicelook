f6624766dbdcccb132ab3429129ab172dfaba016
Kevin Felipe Bolaños Orozco <166841728+FelipeK57@users.noreply.github.com>
2024-10-12 14:12:59 -0500

    Cambio en serializers para obtener los datos necesarios en el endpoint de obtener citas para el calendario

--- employee/serializers.py
+++ employee/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Employee
+from django.contrib.auth.models import User
+
+class userSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
 
 class employeeSerializer(serializers.ModelSerializer):
+    user = userSerializer()
     class Meta:
         model = Employee
         fields = '__all__'
