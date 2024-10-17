d33edf10c9b7070cc87bdafa78d02ff87ffe8dff
Kevin Felipe Bolaños Orozco <166841728+FelipeK57@users.noreply.github.com>
2024-10-16 19:34:55 -0500

    Cambios en los serializers para obtener bien la información

--- employee/serializers.py
+++ employee/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from .models import Employee
-from nicelook_api.serializers import UserSerializer
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
 
 class employeeSerializer(serializers.ModelSerializer):
     user = UserSerializer()
