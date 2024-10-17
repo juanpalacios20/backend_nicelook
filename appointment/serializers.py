0b6ca2687128b1ad3a7f72f160e2361b0667819f
juanpalacios20 <juan.palacios.casanova@correounivalle.edu.co>
2024-10-16 19:35:03 -0500

    Solucionando errores

--- appointment/serializers.py
+++ appointment/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Appointment
 from client.serializers import clientSerializer
-from employee.serializers import employeeSerializer
+from employee.serializers import EmployeeSerializer
 from payment.serializers import paymentSerializer
 from schedule.serializers import scheduleSerializer
 from service.serializers import serviceSerializer
@@ -9,7 +9,7 @@ from establisment.serializers import establismentSerializer
 
 class appointmentSerializer(serializers.ModelSerializer):
     client = clientSerializer()
-    employee = employeeSerializer()
+    employee = EmployeeSerializer()
     payment = paymentSerializer()
     services = serviceSerializer(many=True)
     schedule = scheduleSerializer()
