c7cd234e33499bebc05b84177ea560271f630d8e
juanpalacios20 <juan.palacios.casanova@correounivalle.edu.co>
2024-10-16 20:26:12 -0500

    eliminando rastro del error product_quantity

--- product_payment/admin.py
+++ product_payment/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import Product_payment, Product_quantity
+from .models import Product_payment
 # Register your models here.
 admin.site.register(Product_payment)
-admin.site.register(Product_quantity)
--- product_payment/serializers.py
+++ product_payment/serializers.py
@@ -1,12 +1,7 @@
 from rest_framework import serializers
-from .models import Product_payment, Product_quantity
+from .models import Product_payment
 
 class productPaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product_payment
-        fields = '__all__'
-
-class productQuantitySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product_quantity
         fields = '__all__'
