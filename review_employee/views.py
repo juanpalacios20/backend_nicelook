7dd56c5c7ad3639921cb8638c23116d7bc57ff19
JuanDi <juan.leon.reyes@correounivalle.edu.co>
2024-11-27 16:40:49 -0500

    Update Reviews

--- review_employee/views.py
+++ review_employee/views.py
@@ -49,6 +49,41 @@ def create_review(request, client_id, employee_id, appointment_id):
         return Response({'error': 'Invalid rating format'}, status=status.HTTP_400_BAD_REQUEST)
     
     
+@api_view(['PUT'])
+def update_review(request, client_id, employee_id, appointment_id):
+    
+    try:
+        appointment = Appointment.objects.get(id=appointment_id)
+        employee = Employee.objects.get(id=employee_id)
+        autor = Client.objects.get(id=client_id)
+        
+        if not ReviewEmployee.objects.filter(autor=autor, appointment=appointment).exists():
+            return Response({'error': 'You have not reviewed this appointment'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        review = ReviewEmployee.objects.get(autor=autor, appointment=appointment)
+        
+        comment = request.data.get('comment')
+        rating = request.data.get('rating')
+        
+        if rating is not None and not (0 <= float(rating) <= 5):
+            return Response({'error': 'Rating must be between 0 and 5'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        review.comment = comment
+        review.rating = rating
+        review.save()
+        
+        return Response({'success': 'Review updated'}, status=status.HTTP_200_OK)
+    
+    except Appointment.DoesNotExist:
+        return Response({'error': 'Appointment not found'}, status=status.HTTP_404_NOT_FOUND)
+    except Employee.DoesNotExist:
+        return Response({'error': 'Employee not found'}, status=status.HTTP_404_NOT_FOUND)
+    except Client.DoesNotExist:
+        return Response({'error': 'Client not found'}, status=status.HTTP_404_NOT_FOUND)
+    except ValueError:
+        return Response({'error': 'Invalid rating format'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    
     
 @api_view(['GET'])
 def get_reviews_client(request, client_id):
