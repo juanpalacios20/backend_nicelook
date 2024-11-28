9a6c464e51dfcda7fb72913d8696642693d148b3
Kevin Felipe Bolaños Orozco <kevinboor5725@gmail.com>
2024-11-28 17:00:46 -0500

    Add service duration management and update URL patterns for employee services

--- establisment/urls.py
+++ establisment/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('update-establisment/<int:establisment_id>/', update_establisment, name='update-establisment'),
     path('get-establisment/<int:establisment_id>/', get_establisment, name='get-establisment'),
     path('get-filter-payments-service/<int:establisment_id>/', get_filter_payments_service, name='get-filter-payments-service'),
-    path('servicesByEstablisment/<int:establisment_id>/', views.servicesByEstablisment, name='servicesByEstablisment'),
+    path('servicesByEstablisment/<int:employee_id>/', views.servicesByEstablisment, name='servicesByEstablisment'),
     path('info_establishment/', views.getInfoEstablisment),
     path('get_info_employee/', views.getInfoEmployee),
     path('get_employees/', views.getEmployees),
