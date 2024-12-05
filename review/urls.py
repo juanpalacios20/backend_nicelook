231de69a247101d0822cbb98663e1c187ee16b8a
JuanDi <juan.leon.reyes@correounivalle.edu.co>
2024-12-04 22:32:19 -0500

    Add review endpoints for creating and retrieving reviews

--- review/urls.py
+++ review/urls.py
@@ -8,4 +8,6 @@ router.register('review', views.reviewViewSet, 'review.views')
 
 urlpatterns = [
     path('all/', include(router.urls)),
+    path('create_review/<int:client_id>/<int:establisment_id>/', views.create_review),
+    path('get_reviews/<int:establisment_id>/', views.get_reviews)
 ]
